c323de1f46f8e44d54c1304f9292c37f6b94c982
Mostofa Rezvi <mostofa.aminur.rezvi@gmail.com>
2025-10-22 11:53:34 +0600

    305 - Assignment 3,4,5

--- 2nd Semester/Mid/305 - Web Technology and Internet Computing (Rezvi & Tawhid)/css_assignment/Assignment 5/Calculator.js
+++ 2nd Semester/Mid/305 - Web Technology and Internet Computing (Rezvi & Tawhid)/css_assignment/Assignment 5/Calculator.js
@@ -1,29 +1,55 @@
 const display = document.getElementById("display");
+const calculator = document.querySelector(".calculator");
+let isCalculated = false;
+
+function getRandomColor() {
+  const letters = "0123456789ABCDEF";
+  let color = "#";
+  for (let i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
+  }
+  return color;
+}
 
 function appendInput(value) {
-  if (display.value === "0" && value !== ".") {
+  if (isCalculated && !["+", "-", "*", "/"].includes(value)) {
     display.value = value;
+    isCalculated = false;
   } else {
-    display.value += value;
+    if (display.value === "0" && value !== ".") {
+      display.value = value;
+    } else {
+      display.value += value;
+    }
+    isCalculated = false;
   }
 }
 
 function clearDisplay() {
   display.value = "";
+  isCalculated = false;
 }
 
 function deleteLast() {
   display.value = display.value.toString().slice(0, -1);
+  isCalculated = false;
 }
 
 function calculateResult() {
   try {
     const expression = display.value.replace(/×/g, "*");
-
     const result = new Function("return " + expression)();
-
     display.value = Number(result.toFixed(10));
+    isCalculated = true;
   } catch (error) {
     display.value = "Error";
+    isCalculated = true;
   }
+
+  const color1 = getRandomColor();
+  const color2 = getRandomColor();
+
+  // calculator.style.backgroundColor = getRandomColor();
+
+  document.body.style.background = `linear-gradient(135deg, ${color1}, ${color2})`;
 }
